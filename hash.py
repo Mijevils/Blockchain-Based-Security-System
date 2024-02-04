f0488f122f95e6f8e04e00a0d17fb8b1ffed3a44
Mijevils <75458177+Mijevils@users.noreply.github.com>
2024-02-04 13:34:07 +0000

    Superhash
    Code produces a superhash as a product of all hashes.

--- hash.py
+++ hash.py
@@ -2,7 +2,7 @@ import hashlib
 import os
 
 path = r'C:\Windows\System32\WDI\LogFiles\StartupInfo'
-
+superhash = "" # hashstring containing all file hashes
 for filename in os.listdir(path):
     f = os.path.join(path, filename)
     # checking if it is a file
@@ -15,7 +15,11 @@ for filename in os.listdir(path):
         sha256 = hashlib.sha256()
 
         sha256.update(content)
-
+        superhash += sha256.hexdigest() # hash for this file added to hashstring
         print('#############################################')
         print('{}: {}'.format(sha256.name, sha256.hexdigest()))
         print()
+
+sha256 = hashlib.sha256()
+sha256.update(superhash.encode('utf-8')) # creation of superhash with hashstring
+print(sha256.hexdigest())
