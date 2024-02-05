959d07003d4bbdc0cdd0f0d50c9e1384254b09c6
Mijevils <75458177+Mijevils@users.noreply.github.com>
2024-02-05 11:33:08 +0000

    Class Conversion
    Converted GUI file to a class. Added logo image.

--- GUI.py
+++ GUI.py
@@ -2,47 +2,43 @@ import tkinter as tk
 from tkinter import *
 from PIL import Image, ImageTk
 
-root = tk.Tk()
-root.geometry("900x600")
-basecolour = "#a5e07e"
-root.configure(bg=basecolour)
+class GUI():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("900x600")
+        self.basecolour = "#a5e07e"
 
-def login():
-    print("Test")
+    def makeScene(self):
+        frame = tk.Frame(master=self.root, bg=self.basecolour)
+        frame.pack(pady=0, padx=0, fill="both", expand=True)
 
-frame = tk.Frame(master=root, bg=basecolour)
-frame.pack(pady=0, padx=0, fill="both", expand=True)
+        left_frame = tk.Frame(master=frame, bg="white", width=100)
+        left_frame.pack(side="left", fill="y")
+        top_frame = tk.Frame(master=frame, bg="white", height=75)
+        top_frame.pack(side="top", fill="x", anchor="nw")
 
-left_frame = tk.Frame(master=frame, bg="white", width=100)
-left_frame.pack(side="left", fill="y")
-top_frame = tk.Frame(master=frame, bg="white", height=75)
-top_frame.pack(side="top", fill="x", anchor="nw")
+        label = tk.Label(master=top_frame, text="Protection program", font=("Roboto", 24), bg="white")
+        label.place(relx=0.5, rely=0.5, anchor=CENTER)
 
-label = tk.Label(master=top_frame, text="Protection program", font=("Roboto", 24), bg="white")
-label.pack(pady=12, padx=10)
+        status = tk.Label(master=frame, text="Your device is protected!", font=("Roboto", 24), bg=self.basecolour)
+        status.pack(pady=12, padx=10)
 
-status = tk.Label(master=frame, text="Your device is protected!", font=("Roboto", 24), bg=basecolour)
-status.pack(pady=12, padx=10)
+        image = Image.open('profile.png').resize((100, 100), Image.ANTIALIAS)
+        prof = ImageTk.PhotoImage(image)
+        profile = tk.Button(master=left_frame, image=prof, borderwidth=0, height=100, width=100)
+        profile.pack(pady=10)
 
-image = Image.open('profile.png')
-image = image.resize((100, 100), Image.ANTIALIAS)
-prof = ImageTk.PhotoImage(image)
-profile = tk.Button(master=left_frame, image=prof, borderwidth=0, height=100, width=100)
-profile.pack(pady=10)
+        logo_image = Image.open("logo.png").resize((100, 100), Image.ANTIALIAS)
+        prep = ImageTk.PhotoImage(logo_image)
+        logo = tk.Label(master=top_frame, image=prep, borderwidth=0, height=100, width=100)
+        logo.pack(side=RIGHT)
 
-#entry1 = tk.Entry(master=frame)
-#entry1.insert(0, "Username")  # Set default text as a placeholder
-#entry1.pack(pady=12, padx=10)
+        files = tk.Label(master=left_frame, text="Files in custody:", font=("Roboto", 12), bg="white")
+        files.pack(pady=10, padx=10)
+        uphist = tk.Label(master=left_frame, text="Upload History:", font=("Roboto", 12), bg="white")
+        uphist.pack(pady=200)
 
-#entry2 = tk.Entry(master=frame, show="*")
-#entry2.insert(0, "Password")  # Set default text as a placeholder
-#entry2.pack(pady=12, padx=10)
+        self.root.mainloop()
 
-#button = tk.Button(master=frame, text="Login", command=login)
-#button.pack(pady=12, padx=10)
-
-#checkbox_var = tk.BooleanVar()
-#checkbox = tk.Checkbutton(master=frame, text="Remember me", variable=checkbox_var)
-#checkbox.pack(pady=12, padx=10)
-
-root.mainloop()
+a = GUI()
+a.makeScene()
