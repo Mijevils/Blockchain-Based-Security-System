2c08176c1fd18421eaeb28d94563180055c0ef96
Mijevils <75458177+Mijevils@users.noreply.github.com>
2024-02-05 10:00:53 +0000

    Class update
    Code has been put into a class called Encrypt

--- hash.py
+++ hash.py
@@ -1,25 +1,45 @@
 import hashlib
 import os
 
-path = r'C:\Windows\System32\WDI\LogFiles\StartupInfo'
-superhash = "" # hashstring containing all file hashes
-for filename in os.listdir(path):
-    f = os.path.join(path, filename)
-    # checking if it is a file
-    if os.path.isfile(f):
-        print(f)
+class Encrypt():
+    def __init__(self):
+        self.superhash = "" # string containing all hashes that will then be converted into a final superhash
+        self.sha = hashlib.sha256() # defines the hashing function (sha256)
 
-    fullpath = path + '\\' + filename
-    with open(fullpath, 'rb') as opened_file:
-        content = opened_file.read()
-        sha256 = hashlib.sha256()
+    def makeHash(self, path):
+        """
+        Description: Makes a superhash combining hashes of all files within a given a directory
+        Inputs: Self, as well as a string path containing the path of the directory to hash
+        Output: The superhash, as a string
+        """
+        path = r'C:\Windows\System32\WDI\LogFiles\StartupInfo'
+        for filename in os.listdir(path):  # iterate through all files in given directory
+            f = os.path.join(path, filename)
+            # checking if it is a file
+            if os.path.isfile(f):
+                print(f)  # if file exists, print the name
 
-        sha256.update(content)
-        superhash += sha256.hexdigest() # hash for this file added to hashstring
-        print('#############################################')
-        print('{}: {}'.format(sha256.name, sha256.hexdigest()))
-        print()
+            fullpath = path + '\\' + filename  # determine full path of the file for hash creation
+            with open(fullpath, 'rb') as opened_file:
+                content = opened_file.read()
+                self.sha.update(content)  # hash computed based on file content
+                self.superhash += self.sha.hexdigest()  # hash for this file added to hashstring
+                print('#############################################')
+                print('{}: {}'.format(self.sha.name, self.sha.hexdigest()))
+                print()
 
-sha256 = hashlib.sha256()
-sha256.update(superhash.encode('utf-8')) # creation of superhash with hashstring
-print(sha256.hexdigest())
+        return self.superhash()
+
+    def superhash(self):
+        """
+        Description: Auxiliary function to calculate the hash of the hashstring
+        Input: self
+        Output: String containing the superhash
+        """
+        self.sha.update(self.superhash.encode('utf-8')) # creation of superhash with hashstring
+        print(self.sha.hexdigest())
+
+        return self.sha.hexdigest()
+        
+a = Encrypt()
+a.makeHash("")
