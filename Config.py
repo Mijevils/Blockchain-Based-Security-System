551097622834a99467b7aaa3b59e2f80c4e60e0c
Mijevils <75458177+Mijevils@users.noreply.github.com>
2024-02-12 13:52:11 +0000

    Config Update
    Addition of config file and config-handling code.

--- Config.py
+++ Config.py
@@ -3,13 +3,48 @@ import os
 
 
 class Config():
-    def __init__(self):
-        self.config = {}
-
-    def makeConfig(self, data):
-        if (os.path.isdir('config.json')):
-            with open ('config.json', w) as f:
-                json.dump(data, f)
+    def readConfig(self):
+        """
+        Description: Function to read config.json and determine what directories should be under protection
+        Input: Self
+        Return: The content of config.json
+        """
+        if os.path.isfile('config.json'):
+            with open ('config.json', 'rb') as file:
+                if (os.stat("config.json").st_size == 0):
+                    return {"paths" : []}
+                else:
+                    return json.load(file)
         else:
-            with open('config.json', 'rb') as f:
+            print("No file named config.json")
+
+    def makeConfig(self):
+        if not (os.path.isfile('config.json')):
+            with open('config.json', 'x') as f:
                 self.config = f
+        else:
+            print("File config.json already exists.")
+
+    def writeConfig(self, path):
+        if not os.path.isfile("config.json"):
+            self.makeConfig()
+
+        data = self.readConfig()
+        data["paths"].append(path)
+
+        with open('config.json', 'w') as file:
+            json.dump(data,file)
+
+    def clearConfig(self):
+        if os.path.isfile("config.json"):
+            with open('config.json', "r+") as file:
+                file.truncate(0)
+        else:
+            print("No file named config.json")
+
+    def getProtecteds(self):
+        data = self.readConfig()
+        return data["paths"]
+
+#a = Config()
+#a.getProtecteds()
